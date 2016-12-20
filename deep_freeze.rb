bcfa8928df47e9e10325129b726826a6c1ae08ec
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2016-12-20 15:27:15 -0500

    fix errors

--- deep_freeze.rb
+++ deep_freeze.rb
@@ -3,9 +3,13 @@ class Object
   def deep_freeze
 
     #  Recursive calling #deep_freeze for enumerable objects.
-     if self.respond_to? :each
-       self.each { |val| val.deep_freeze }
-     end
+    if self.respond_to? :each
+      if self.class == Hash
+        self.each { |key, val| val.deep_freeze }
+      else
+        self.each { |val| val.deep_freeze }
+      end
+    end
 
     #  Freezing of all instance variable values.
     self.instance_variables.each do |var|
@@ -14,23 +18,30 @@ class Object
       self.instance_variable_set(var, frozen_val)
     end
 
-    self.freeze # Freezing of an element
+    self.freeze unless self.frozen? # Freezing of an element.
   end # End of #deep_freeze
 
   def deep_frozen?
+
     #  Recursive calling #deep_freeze for enumerable objects.
     if self.respond_to? :each
-      self.each { |val| val.deep_frozen? }
+      if self.class == Hash
+        flag = self.all? { |key, val| val.deep_frozen? }
+      else
+        flag = self.all? { |val| val.deep_frozen? }
+      end
+    else
+      flag = true
     end
 
     #  #frozen? for all instance variable values.
-    flag = self.frozen?
+    flag = self.frozen? if flag
     if flag == true
       flag = self.instance_variables.all? do |var|
         val = self.instance_variable_get(var)
-        val.frozen?
+        val.deep_frozen?
       end
     end
-    fl # returning flag.
+    flag # returning flag.
   end
 end # End of Object class.
